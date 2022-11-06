b58f9b2e578e6e8369c17dafe58a703e03e7a9ad
nem0z <maxime.bernard.dev@gmail.com>
2022-11-06 18:07:38 +0100

    Feat (blockchain/storage) Refractor blockchain to persist block data locally

--- blockchain/blockchain/blockchain.go
+++ blockchain/blockchain/blockchain.go
@@ -2,49 +2,86 @@ package blockchain
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/nem0z/go-blockchain/blockchain/block"
 )
 
 type Blockchain struct {
-	Blocks []*block.Block
+	Database *Storage
 }
 
-func New() *Blockchain {
-	genesis := block.Genesis()
-	if err := genesis.Validate(); err != nil {
-		log.Panic(err)
+func New() (error, *Blockchain) {
+	err, storage := InitDB()
+	if err != nil {
+		return err, &Blockchain{}
 	}
 
-	return &Blockchain{Blocks: []*block.Block{genesis}}
+	bc := &Blockchain{Database: storage}
+
+	err, _ = bc.Database.Load()
+
+	if err != nil {
+		genesis := block.Genesis()
+		if err := genesis.Validate(); err != nil {
+			return err, &Blockchain{}
+		}
+
+		err := bc.Add(genesis)
+		if err != nil {
+			return err, &Blockchain{}
+		}
+	}
+
+	return nil, bc
 }
 
-func (bc *Blockchain) Display() {
-	for _, b := range bc.Blocks {
+func (bc *Blockchain) Display() error {
+	err, iter := bc.Iterator()
+
+	if err != nil {
+		return err
+	}
+
+	for {
+		err, b := iter.Next()
+
+		if err != nil {
+			return err
+		}
+
 		fmt.Println("-----*-----")
 		fmt.Printf("Hash => %x\n", b.Hash)
 		fmt.Printf("Data => %s\n", b.Data)
+
+		if b.PrevHash == (block.Hash{}) {
+			break
+		}
 	}
+
 	fmt.Println("-----*-----")
+	return nil
 }
 
-func (bc *Blockchain) Last() *block.Block {
-	return bc.Blocks[len(bc.Blocks)-1]
+func (bc *Blockchain) LastHash() (error, block.Hash) {
+	return bc.Database.Load()
 }
 
-func (bc *Blockchain) AddBlock(b *block.Block) error {
+func (bc *Blockchain) Add(b *block.Block) error {
 
 	if err := b.Validate(); err != nil {
 		return err
 	}
 
-	bc.Blocks = append(bc.Blocks, b)
+	bc.Database.Add(b)
 	return nil
 }
 
 func (bc *Blockchain) CreateAndAdd(data string) error {
-	newBlock := &block.Block{Data: []byte(data), PrevHash: bc.Last().PrevHash}
+	err, lastHash := bc.LastHash()
+	if err != nil {
+		return err
+	}
 
-	return bc.AddBlock(newBlock)
+	newBlock := &block.Block{Data: []byte(data), PrevHash: lastHash}
+	return bc.Add(newBlock)
 }
